a7640aed23fe3993c28d32db917f4b1aa38ff188
WelshSP <69017191+WelshSP@users.noreply.github.com>
2020-07-31 00:38:01 -0700

    Add files via upload

---
+++ src/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="navbar">
+    <div >
+        <img class="img" alt="Wilbert logo" src="../assets/logo.png">
+    </div>
+    <div>
+        <a href="./components/home.vue">Home</a>
+        <a href="./components/products.vue">Products</a>
+        <a href="./components/genesisgranite.vue">Genesis Granite</a>
+        <a href="./components/aboutus.vue">About Us</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+   export default{
+
+   }
+</script>
+
+<style>
+    .navbar {
+        width: 100%;
+        background-color: white;
+        overflow: auto;
+    }
+
+    .navbar a {
+
+        padding: 3em;
+        color: black;
+        text-decoration: none;
+        font-size: 17px;
+        position: relative;
+    }
+
+    .navbar a:hover {
+        background-color: rgb(120, 171, 236);
+    }
+
+    .active {
+        background-color: #2449c4;
+    }
+
+    @media screen and (max-width: 500px) {
+        .navbar a {
+            float: none;
+            display: block;
+        }
+    }
+    .img {
+        float: left;
+        position: relative;
+        height: 75px;
+        width: auto;
+    }
+</style>
---
+++ src/TopHeader.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="navbar">
+        <div>
+            <a href="">Request a Quote</a>
+            <a href="">Pay an Invoice</a>
+            <a href="">Customer Portal</a>
+            <a href="">Contact</a>
+        </div>
+  </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .navbar {
+        width: 100%;
+        background-color: lightgray;
+        overflow: auto;
+        position: fixed;
+        top: 0;
+        width: 100%;
+
+    }
+
+    .navbar a {
+        float: right;
+        text-align: justify;
+        padding: .5em;
+        color: black;
+        text-decoration: none;
+        font-size: 12px;
+    }
+
+    .navbar a:hover {
+        background-color: rgb(120, 171, 236);
+    }
+
+    .active {
+        background-color: #2449c4;
+    }
+</style>
